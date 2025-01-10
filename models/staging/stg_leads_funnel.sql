4c405008582b540c167f92883b15fbb6e1d759d2
Gonzalo Hernandez <gonzalohernandez1990@gmail.com>
2025-01-10 14:46:58 +0100

    New dbt files

---
+++ models/staging/stg_leads_funnel.sql
@@ -0,0 +1,18 @@
+WITH cleaned_leads_funnel AS (
+    SELECT
+        DATE AS lead_activity_date,
+        COUNTRY_CODE AS country_code,
+        CAMPAIGN_ID AS campaign_id,
+        TOTAL_IMPRESSIONS AS total_impressions,
+        TOTAL_CLICKS AS total_clicks,
+        TOTAL_SPEND AS total_spend,
+        TOTAL_LEADS AS total_leads,
+        TOTAL_FAKE_LEADS AS total_fake_leads,
+        TOTAL_SQLS AS total_sqls,
+        TOTAL_MEETING_DONE AS total_meeting_done,
+        TOTAL_SIGNED_LEADS AS total_signed_leads,
+        TOTAL_POS_LITE_DEALS AS total_pos_lite_deals
+    FROM {{ source('raw_data', 'leads_funnel') }}
+)
+
+SELECT * FROM cleaned_leads_funnel
